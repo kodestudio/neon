78196ae24f402403dc0a78fc845780196f333dfb
quocthinhvo <quocthinhvo0@gmail.com>
2020-08-15 18:40:46 +0700

    Added GitHub get code and status with custom and events

---
+++ js/github.js
@@ -0,0 +1,56 @@
+// This file to get data from GitHub API
+/*
+    Author: võ Quốc Thịnh
+    GitHub: quocthinhvo
+    Email: quocthinhvo0@gmail.com
+*/
+
+// var GitHub_api = new XMLHttpRequest();
+
+var parent_value = config_parent;
+var repo_value = config_repo;
+
+var GitHub_getCustom = {
+    code: function (type, url, ex) {
+        var api = new XMLHttpRequest();
+        api.open(type, url, ex);
+        api.send(null);
+        return api.status;
+    },
+    text: function (type, url, ex) {
+        var api = new XMLHttpRequest();
+        api.open(type, url, ex);
+        api.send(null);
+        return api.responseText;
+    }
+}
+
+var GitHub_getInfo = {
+    code: function(){
+        var api = new XMLHttpRequest();
+        api.open("GET", "https://api.github.com/repos/" + parent_value + "/" + repo_value, false);
+        api.send(null);
+        return api.status;
+    },
+    text: function(){
+        var api = new XMLHttpRequest();
+        api.open("GET", "https://api.github.com/repos/" + parent_value + "/" + repo_value, false);
+        api.send(null);
+        return api.responseText;
+    }
+}
+
+var GitHub_getEvents = {
+    code: function(){
+        var api = new XMLHttpRequest();
+        api.open("GET", "https://api.github.com/repos/" + parent_value + "/" + repo_value + "/events", false);
+        api.send(null);
+        return api.status;
+    },
+    text: function(){
+        var api = new XMLHttpRequest();
+        api.open("GET", "https://api.github.com/repos/" + parent_value + "/" + repo_value + "/events", false);
+        api.send(null);
+        return api.responseText;
+    }
+}
