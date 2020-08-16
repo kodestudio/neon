b95a8c39ca2463e21e17c7e9a661e2fc256896e7
quocthinhvo <quocthinhvo0@gmail.com>
2020-08-16 22:56:16 +0700

    Change style

--- js/index.js
+++ js/index.js
@@ -15,4 +15,6 @@ function init(){
     document.getElementById("owner_avatar").src = obj_repo.owner.avatar_url;
     // change repo name
     document.getElementById("repo_name").innerHTML = obj_repo.full_name;
+    //change description of repo
+    document.getElementById("repo_desc").innerHTML = obj_repo.description;
 }
