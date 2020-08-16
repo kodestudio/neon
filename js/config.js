fc8784b9109c6fe89036310de6b9b0dfffb42217
quocthinhvo <quocthinhvo0@gmail.com>
2020-08-16 23:10:42 +0700

    Change "var" bt "const" in config.js

--- js/config.js
+++ js/config.js
@@ -14,21 +14,21 @@ GitHub: quocthinhvo
 Email: quocthinhvo0@gmail.com
 */
 
-var config_key = "value";
+const config_key = "value";
 
 // Name of org or owner repo
 // if url to repo = "https://www.github.com/kodestudio/neon" then parent = "kodestudio" and repo="neon"
-var config_parent = "kodestudio";
-var config_repo = "neon";
-var config_title = "Title page here";
+const config_parent = "kodestudio";
+const config_repo = "neon";
+const config_title = "Title page here";
 
 // config type event load
 /*
-var config_issues = true;
-var config_isuecomment = true;
-var config_release = true;
-var config_push = true;
-var config_pull = true;
+const config_issues = true;
+const config_isuecomment = true;
+const config_release = true;
+const config_push = true;
+const config_pull = true;
 */
 // config of custom Data
-var config_customapi = "";
+const config_customapi = "";
