51715963ca10515d1db2e67a938aee4de301599a
quocthinhvo <quocthinhvo0@gmail.com>
2020-08-17 22:30:42 +0700

    Added param (obj and i)

--- js/event.js
+++ js/event.js
@@ -17,54 +17,54 @@ function event_get(){
     for (var i = 0; i < obj_events.length; i++){
         switch (obj_events[i].type){
             case "IssuesEvent":
-                if (config_show_issues) event_Issue();
+                if (config_show_issues) event_Issue(obj_events, i);
             break;
             case "PushEvent":
-                if (config_show_push) event_Push();
+                if (config_show_push) event_Push(obj_events, i);
             break;
             case  "IssueCommentEvent":
-                if (config_show_isuecomment) event_CommentIssue();
+                if (config_show_isuecomment) event_CommentIssue(obj_events, i);
             break;
             case "PullRequestEvent":
-                if (config_show_pull) event_Pull();
+                if (config_show_pull) event_Pull(obj_events, i);
             break;
             case "ForkEvent":
-                if (config_show_fork) event_Fork();
+                if (config_show_fork) event_Fork(obj_events, i);
             break;
             case "GollumEvent":
-                if (config_show_wiki) event_Gollum();
+                if (config_show_wiki) event_Gollum(obj_events, i);
             break;
             case "ReleaseEvent":
-                if (config_show_release) event_Release();
+                if (config_show_release) event_Release(obj_events, i);
             break;  
         }
     }
 }
 
-function event_Issue(){
+function event_Issue(input, i){
+    
+}
+
+function event_CommentIssue(input, i){
 
 }
 
-function event_CommentIssue(){
+function event_Pull(input, i){
 
 }
 
-function event_Pull(){
+function event_Fork(input,i){
 
 }
 
-function event_Fork(){
+function event_Gollum(input,i){
 
 }
 
-function event_Gollum(){
+function event_Release(input,i){
 
 }
 
-function event_Release(){
-
-}
-
-function event_Push(){
-
+function event_Push(input, i){
+    console.log(input[i].type)
 }
